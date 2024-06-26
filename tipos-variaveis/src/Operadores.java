773e48c44fd5fb962080e7e10f809842e944219b
GUI1312SM <gui1312@live.com>
2024-06-25 22:30:40 -0300

    SmartTV

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && ( 7 > 4 ) ){
+            System.out.println("as duas são verdadeiras");
+        }
+       
+        if(condicao1 || condicao2){
+            System.out.println("Uma da condicões é verdadeira");
+        }
+        System.out.println("fim");
+       
+    }    
+}
