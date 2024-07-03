06c94f7f06de740ee3c8ddb41ffb52a239230fd7
GUI1312SM <gui1312@live.com>
2024-07-03 15:57:57 -0300

    Conta Banco

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,7 @@
 public class Operadores {
     public static void main(String[] args) {
         boolean condicao1 = true;
-
+        
         boolean condicao2 = false;
 
         if(condicao1 && ( 7 > 4 ) ){
